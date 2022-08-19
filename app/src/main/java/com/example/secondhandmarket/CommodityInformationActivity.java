3b0de1fe5020c30f928835ef7fa64032087d084c
SmLin <2775147476@qq.com>
2022-08-20 04:06:10 +0800

    pp

---
+++ app/src/main/java/com/example/secondhandmarket/CommodityInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.secondhandmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommodityInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commodity_information);
+    }
+}
