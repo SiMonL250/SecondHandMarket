1ae4b50d81a2d4db57849be23a5d5fe839f4b881
SmLin <2775147476@qq.com>
2022-10-14 17:04:58 +0800

    final

--- app/src/main/java/com/example/secondhandmarket/appkey/appMobSDK.java
+++ app/src/main/java/com/example/secondhandmarket/appkey/appMobSDK.java
@@ -1,8 +1,8 @@
 package com.example.secondhandmarket.appkey;
 
 public final class appMobSDK {
-    public String appID = "1c43b58c794448edba2992f973ff2120";
-//            "6e7ad529141b4ec18c355eff7abfd160";
-    public String appSecret = "7428391e8fb7741f3458db716391b5526510b";
-//        "63421994d54e2abe54902b678072a31a94e66";
+    public String appID =
+            "6e7ad529141b4ec18c355eff7abfd160";
+    public String appSecret =
+        "63421994d54e2abe54902b678072a31a94e66";
 }
